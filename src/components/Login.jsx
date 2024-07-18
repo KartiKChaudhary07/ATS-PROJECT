3e89facda2b5171b47e4191b277ae5c695845667
Kartikey Pandey <122464491+kartkp@users.noreply.github.com>
2024-07-19 02:51:52 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -13,7 +13,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post('http://localhost:5000/api/auth/login', { email, password });
+      const res = await axios.post('http://localhost:3000/login', { email, password });
       if (res.data.token) {
         localStorage.setItem('token', res.data.token);
         navigate('/home');
